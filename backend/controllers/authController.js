0d9ea0402e7935ec7b3b8aa54cca525f518b7ce7
elion-Tech <staff.elionservices@gmail.com>
2026-04-09 14:09:04 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,7 +13,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production', // Only send over HTTPS in production
     sameSite: 'none'
   };
 
@@ -80,7 +80,7 @@ export const logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production', // Only send over HTTPS in production
     sameSite: 'none'
   });
   res.status(200).json({ status: 'success' });
